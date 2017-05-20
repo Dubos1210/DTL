0029624768866b395c6fa945d63b6eef2c28efcc
Vladimir Dubos <dubos1210@yandex.ru>
2017-05-20 20:38:12 +0300

    DTL

---
+++ dtl/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace dtl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Candara", 25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No Key";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 502);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DTL utility";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ dtl/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Media;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace dtl
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap image;
+        public string path = "0.wav";
+        void play()
+        {
+            //SoundPlayer Sound = new SoundPlayer(Application.StartupPath +@"\"+path);
+
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = Application.StartupPath + @"\" + path;
+            try
+            {
+                //Sound.Play();
+                player.Play();
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            label1.Text = e.KeyChar.ToString();
+            path = e.KeyChar.ToString() + ".wav";
+            pictureBox1.Visible = true;
+            try
+            {
+                pictureBox1.Image = image;
+                image = new Bitmap(e.KeyChar.ToString() + ".png", true);
+                pictureBox1.Image = image;
+            }
+            catch (System.ArgumentException ex)
+            {
+                label1.Text = "Error";
+                pictureBox1.Visible = false;
+            }
+
+            Thread thr = new Thread(play);
+            thr.Start();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            label1.Text = "No Key";
+            pictureBox1.Visible = false;
+        }
+    }
+}
